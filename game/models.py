4df001bc0275cbde1ef56c923915814d6e83b29f
jvmaia <joaovmferreira@gmail.com>
2017-12-10 22:48:15 -0200

    improve models

--- game/models.py
+++ game/models.py
@@ -1,17 +1,63 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import JSONField, ArrayField
 
-class effect(models.Model):
+class Effect(models.Model):
 	name = models.CharField(max_length=20)
 	consequences = JSONField()
+	description = models.CharField(max_length=100)
 
-class skill(models.Model):
+class Skill(models.Model):
 	name = models.CharField(max_length=20)
 	duration = models.IntegerField()
-	effects = models.ManyToManyField(effect)
+	effects = models.ManyToManyField(Effect)
+	description = models.CharField(max_length=100)
 
-class breed(models.Model):
+class Breed(models.Model):
 	name = models.CharField(max_length=20)
 	mean_height = models.FloatField()
-	habitat = models.CharField(max_length=20)
-	passive_skill = models.ManyToManyField(skill)
+	passive_skill = models.ManyToManyField(Skill)
+	description = models.CharField(max_length=200)
+
+class Clothes_family(models.Model):
+	name = models.CharField(max_length=20)
+
+class Clothes(models.Model):
+	name = models.CharField(max_length=20)
+	family = models.ForeignKey(
+		'Clothes_family',
+		on_delete=models.SET_NULL,
+		blank=True,
+		null=True
+	)
+	description = models.CharField(max_length=100)
+	weight = models.IntegerField()
+	extras = JSONField()
+
+class Weapons_family(models.Model):
+	name = models.CharField(max_length=20)
+	range = models.IntegerField()
+
+class Weapon(models.Model):
+	choices_hand = (
+		('1', 'One-hand'),
+		('2', 'Two-hands')
+	)
+	name = models.CharField(max_length=20)
+	family = models.ForeignKey(
+		'Weapons_family',
+		on_delete=models.SET_NULL,
+		blank=True,
+		null=True
+	)
+	hands = models.CharField(
+		choices=choices_hand,
+		max_length=1
+	)
+	extras = JSONField()
+
+class Class(models.Model):
+	name = models.CharField(max_length=20)
+	weapons = models.ManyToManyField(Weapons_family)
+	skills = models.ManyToManyField(Skill)
+	clothes = models.ManyToManyField(Clothes_family)
+	description = models.CharField(max_length=200)
--- rpg/settings.py
+++ rpg/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+USER, PASSWORD, HOST, PORT = open('rpg/credentials.txt').read().split()
 
 # Application definition
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'rpg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rpg',
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT
     }
 }
 
