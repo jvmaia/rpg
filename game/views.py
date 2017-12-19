3a00cc635a11ef857911b9b2750f6b7a8f77e605
jvmaia <joaovmferreira@gmail.com>
2017-12-19 16:34:59 -0200

    add view with login_required

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+# TODO create custom authentication_form for our custom user model
+
+@login_required(login_url='login/')
+def dashboard(request):
+	return HttpResponse('<a href="/dash/logout">Logout</a>')
--- rpg/urls.py
+++ rpg/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from game.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dash/', dashboard),
+    path('dash/login/', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
+    path('dash/logout/', auth_views.logout, {'template_name': 'core/logged_out.html'}, name='logout')
 ]
