63e68d49876203a9cade1defc88449a01c6f0434
jvmaia <joaovmferreira@gmail.com>
2017-12-23 23:55:46 -0200

    add page to select a char

--- game/views.py
+++ game/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Char
 from django.contrib.auth.decorators import login_required
 
 # TODO create custom authentication_form for our custom user model
 
 @login_required(login_url='login/')
 def dashboard_master(request):
-	return render(request, "game/dashboard_master.html")
+	return render(request, 'game/dashboard_master.html')
 
 @login_required(login_url='login/')
 def dashboard_player(request):
-    return render(request, "game/dashboard_player.html")
+	try:
+		char = Char.objects.get(name=requests.COOKIES['char'])
+		return render(request, 'game/dashboard_player.html')
+	except:
+		all_chars = Char.objects.all()
+		return render(request, 'game/select_char.html', {'chars': all_chars})
 
 def main_page(request):
-    return render(request, "game/main_page.html")
+    return render(request, 'game/main_page.html')
