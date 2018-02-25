d115085adaa9d43dcc6354b56fbe1244dc217f1e
jvmaia <joaovmferreira@gmail.com>
2018-02-24 22:08:55 -0300

    fix message to levelup a char

--- game/views.py
+++ game/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login/')
 def dashboard_master(request):
     maps = Map.objects.all()
-    chars = Char.objects.all()
+    chars = Char.objects.all().order_by('-level')
     return render(request, 'game/dashboard_master.html', {
         'maps': maps,
         'chars': chars
@@ -38,4 +38,4 @@ def char_levelup(request, char_id):
     char = get_object_or_404(Char, pk=char_id)
     char.level += 1
     char.save()
-    return HttpResponse("Char %s arrived to a new level" % (char_id))
+    return HttpResponse("Char %s arrived to a new level" % (char_id), content_type="text/plain")
